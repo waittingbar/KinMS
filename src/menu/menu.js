4741c07db97598acaa21ff56a6893dd25fe95d10
LiPengFei <demoworld@sina.com>
2018-08-12 17:01:05 +0800

    初始化项目

---
+++ src/menu/menu.js
@@ -0,0 +1,133 @@
+let menu = {};
+
+/**
+ * 首页
+ * @type {{name: string, path: string, icon: string}}
+ */
+menu.home = {
+  name: '首页',
+  path: '/',
+  icon: 'fa fa-tachometer',
+};
+
+
+/**
+ * 学生管理
+ * @type {{name: string, icon: string, children: {}}}
+ */
+menu.font_icon = {
+  name: '学生管理',
+  icon: 'fa fa-th',
+  children: {}
+};
+let icon = menu.font_icon.children;
+
+icon.font_awesome = {
+  name: '学生列表',
+  path: '/font_awesome',
+
+};
+icon.element_icon = {
+  name: '新增学生',
+  path: '/element_icon',
+};
+
+/**
+ * 班级管理
+ * @type {{name: string, icon: string, children: {}}}
+ */
+menu.content_manage = {
+  name: '班级管理',
+  icon: 'fa fa-file-text-o',
+  children: {}
+};
+
+let ContentManage = menu.content_manage.children;
+
+ContentManage.post = {
+  name: '班级列表',
+  path: '/post_manage',
+};
+ContentManage.add = {
+  name: '新增班级',
+  path: '/class_add',
+};
+
+/**
+ * 接送人管理
+ * @type {{name: string, icon: string, children: {}}}
+ */
+menu.user_manage = {
+  name: '接送人管理',
+  icon: 'fa fa-user-circle-o',
+  children: {}
+};
+let UserManage = menu.user_manage.children;
+
+UserManage.user = {
+  name: '接送人列表',
+  path: '/user_manage',
+};
+
+
+/**
+ * 接送记录管理
+ * @type {{name: string, icon: string, children: {}}}
+ */
+menu.category_manage = {
+  name: '接送记录管理',
+  icon: 'fa fa-sitemap',
+  children: {}
+};
+let CategoryManage = menu.category_manage.children;
+
+CategoryManage.daoxiao = {
+  name: '今日到校记录',
+  path: '/category_manage',
+};
+CategoryManage.lixiao = {
+  name: '今日离校记录',
+  path: '/category_manage',
+};
+CategoryManage.jiesong = {
+  name: '接送历史记录',
+  path: '/category_manage',
+};
+
+
+
+menu.permission_manage = {
+  name: '系统管理',
+  icon: 'fa fa-qrcode',
+  children: {}
+};
+let PermissionManage = menu.permission_manage.children;
+
+PermissionManage.role = {
+  name: '帐号列表',
+  path: '/role_manage',
+};
+
+PermissionManage.permission = {
+  name: '新增帐号',
+  path: '/permission_list',
+};
+
+export default menu;
+
+// if(__DEV__){
+//
+//   menu.development_tools = {
+//     name: '开发工具',
+//     icon: 'fa fa-wrench',
+//     children: {}
+//   };
+//
+//   let DevelopmentTools = menu.development_tools.children;
+//
+//   DevelopmentTools.code = {
+//     name: '构建代码',
+//     path: '/build_code',
+//   };
+//
+// }
---
+++ src/router/index.js
@@ -0,0 +1,121 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Personal from './personal'
+
+Vue.use(Router)
+
+
+
+let RouteList = [
+  {
+    path: '/',
+    component: resolve => require(['~/views/Layout/App.vue'], resolve),
+    meta: {
+      title: '首页',
+      keepAlive: false,
+    },
+    children: [
+      {
+        path: '/',
+        name: 'Dashboard',
+        meta: {
+          title: '首页',
+          keepAlive: false
+        },
+        component: resolve => require(['~/views/Home/Index.vue'], resolve),
+      },
+      {
+        path: '/font_awesome',
+        name: 'FontAwesome',
+        meta: {
+          title: '学生管理',
+          keepAlive: false
+        },
+        component: resolve => require(['~/views/Icon/FontAwesome.vue'], resolve),
+      },
+      {
+        path: '/element_icon',
+        name: 'ElementIcon',
+        meta: {
+          title: '班级管理',
+          keepAlive: false
+        },
+        component: resolve => require(['~/views/Icon/ElementIcon.vue'], resolve),
+      },
+      {
+        path: '/post_manage',
+        name: 'PostManage',
+        meta: {
+          title: '接送人管理',
+          keepAlive: false
+        },
+        component: resolve => require(['~/views/ContentManage/Index.vue'], resolve),
+      },
+      {
+        path: '/user_manage',
+        name: 'UserManage',
+        meta: {
+          title: '接送记录管理',
+          keepAlive: true
+        },
+        component: resolve => require(['~/views/UserManage/Index.vue'], resolve),
+      },
+      {
+        path: '/category_manage',
+        name: 'CategoryManage',
+        meta: {
+          title: '系统管理',
+          keepAlive: true
+        },
+        component: resolve => require(['~/views/CategoryManage/Index.vue'], resolve),
+      },
+      {
+        path: '/role_manage',
+        name: 'RoleManage',
+        meta: {
+          title: '角色列表',
+          keepAlive: true
+        },
+        component: resolve => require(['~/views/PermissionManage/Role/Role.vue'], resolve),
+      },
+      {
+        path: '/permission_list',
+        name: 'PermissionList',
+        meta: {
+          title: '权限列表',
+          keepAlive: true
+        },
+        component: resolve => require(['~/views/PermissionManage/Permission/Index.vue'], resolve),
+      },
+      //个人中心，可能有修改密码，头像修改等路由
+      Personal.index,
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      title: '后台登录',
+      keepAlive: false
+    },
+    components: {
+      blank: resolve => require(['~/views/Login/Login.vue'], resolve),
+    }
+  },
+
+]
+
+
+RouteList[0].children.push({
+  path: '/build_code',
+  name: 'BuildCode',
+  meta: {
+    title: '构建代码',
+    keepAlive: true
+  },
+  component: resolve => require(['~/views/DevelopmentTool/Build.vue'], resolve),
+});
+
+export default new Router({routes: RouteList})
+
+
